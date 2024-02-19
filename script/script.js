27efb216e1279b6fcbb1bc90d1a735ebd28ecd52
AHNAYEEM40 <ahnayeem40@gmail.com>
2024-02-19 15:10:27 +0600

    degine almost

--- script/script.js
+++ script/script.js
@@ -1,6 +1,23 @@
-const a = document.getElementsByClassName("seats")
-for (const seat of a) {
-    console.log(seat)
-    
-}
 
+let count = 0;
+const a = document.getElementsByClassName('seats');
+for (const seat of a) {
+  const seatCount = document.getElementById('count');
+  const ticketList = document.getElementById('ticketList');
+  const availableSeat = document.getElementById('availableSeat');
+  const available = parseInt(availableSeat.innerText);
+  seat.addEventListener('click', function (e) {
+    const btnText = e.target.innerText;
+    count += 1;
+    availableSeat.innerText = available - 1;
+    seatCount.innerText = count;
+
+    const createList = document.createElement('div');
+    createList.innerHTML = `<div class="flex space-x-28 text-xl text-center">
+    <p>${btnText}</p>
+    <p>Economoy</p>
+    <p>BDT 550</p>
+  </div>`;
+    ticketList.appendChild(createList);
+  });
+}
